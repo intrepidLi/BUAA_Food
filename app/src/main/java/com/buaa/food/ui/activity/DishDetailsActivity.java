f8853613723bce0c23ad3559588e215862c39e59
彭莘 <psfott@buaa.edu.cn>
2023-11-30 21:14:42 +0800

    please read log.md

---
+++ app/src/main/java/com/buaa/food/ui/activity/DishDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.buaa.food.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.buaa.food.R;
+
+public class DishDetailsActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dishes); // Set your XML layout here
+    }
+}
--- app/src/main/java/com/buaa/food/ui/fragment/HangWeiFragment.java
+++ app/src/main/java/com/buaa/food/ui/fragment/HangWeiFragment.java
@@ -1,9 +1,19 @@
 package com.buaa.food.ui.fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.buaa.food.http.glide.GlideApp;
+import com.buaa.food.ui.adapter.DishAdapter;
 import com.bumptech.glide.load.MultiTransformation;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
@@ -12,8 +22,13 @@ import com.buaa.food.R;
 import com.buaa.food.aop.SingleClick;
 import com.buaa.food.app.TitleBarFragment;
 import com.buaa.food.ui.activity.HomeActivity;
+import com.gyf.immersionbar.ImmersionBar;
 import com.hjq.widget.view.CountdownView;
 import com.hjq.widget.view.SwitchButton;
+
+import java.util.ArrayList;
+
+import javax.xml.transform.sax.SAXResult;
 
 /**
  *    author : Android 轮子哥
@@ -21,13 +36,14 @@ import com.hjq.widget.view.SwitchButton;
  *    time   : 2018/10/18
  *    desc   : 发现 Fragment
  */
+
 public final class HangWeiFragment extends TitleBarFragment<HomeActivity>
         implements SwitchButton.OnCheckedChangeListener {
 
-    private ImageView mCircleView;
-    private ImageView mCornerView;
-    private SwitchButton mSwitchButton;
-    private CountdownView mCountdownView;
+    private Toolbar mToolbar;
+    private EditText mHintView;
+    private AppCompatImageView mSearchView;
+    private RecyclerView recyclerView;
 
     public static HangWeiFragment newInstance() {
         return new HangWeiFragment();
@@ -40,37 +56,47 @@ public final class HangWeiFragment extends TitleBarFragment<HomeActivity>
 
     @Override
     protected void initView() {
-        mCircleView = findViewById(R.id.iv_find_circle);
-        mCornerView = findViewById(R.id.iv_find_corner);
-        mSwitchButton = findViewById(R.id.sb_find_switch);
-        mCountdownView = findViewById(R.id.cv_find_countdown);
-        setOnClickListener(mCountdownView);
+        mToolbar = findViewById(R.id.tb_hangwei_title);
+        mHintView = findViewById(R.id.tv_hangwei_hint);
+        mSearchView = findViewById(R.id.iv_hangwei_search);
+        recyclerView = findViewById(R.id.hangwei_recyclerView);
 
-        mSwitchButton.setOnCheckedChangeListener(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        ArrayList<String> dishes = createDishesList();
+        recyclerView.setAdapter(new DishAdapter(dishes));
+        // 给这个 ToolBar 设置顶部内边距，才能和 TitleBar 进行对齐
+        ImmersionBar.setTitleBar(getAttachActivity(), mToolbar);
+
+        mHintView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
 
     @Override
     protected void initData() {
-        // 显示圆形的 ImageView
-        GlideApp.with(this)
-                .load(R.drawable.update_app_top_bg)
-                .transform(new MultiTransformation<>(new CenterCrop(), new CircleCrop()))
-                .into(mCircleView);
 
-        // 显示圆角的 ImageView
-        GlideApp.with(this)
-                .load(R.drawable.update_app_top_bg)
-                .transform(new MultiTransformation<>(new CenterCrop(), new RoundedCorners((int) getResources().getDimension(R.dimen.dp_10))))
-                .into(mCornerView);
     }
 
     @SingleClick
     @Override
     public void onClick(View view) {
-        if (view == mCountdownView) {
-            toast(R.string.common_code_send_hint);
-            mCountdownView.start();
-        }
+
     }
 
     @Override
@@ -86,5 +112,13 @@ public final class HangWeiFragment extends TitleBarFragment<HomeActivity>
     @Override
     public void onCheckedChanged(SwitchButton button, boolean checked) {
         toast(checked);
+    }
+
+    private ArrayList<String> createDishesList() {
+        ArrayList<String> dishList = new ArrayList<>();
+        for (int i = 1; i <= 12; i++) {
+            dishList.add("菜品名称 " + i);
+        }
+        return dishList;
     }
 }
