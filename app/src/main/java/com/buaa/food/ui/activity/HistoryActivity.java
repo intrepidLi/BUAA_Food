09ac75c2435299f622be9ebf7ae56d563bcb5ecb
叶怀谦 <21371161@buaa.edu.cn>
2023-12-03 16:41:05 +0800

    favorite-history_fix

--- app/src/main/java/com/buaa/food/ui/activity/HistoryActivity.java
+++ app/src/main/java/com/buaa/food/ui/activity/HistoryActivity.java
@@ -1,14 +1,26 @@
 package com.buaa.food.ui.activity;
 
+import androidx.viewpager.widget.ViewPager;
+
 import com.buaa.food.R;
 import com.buaa.food.app.AppActivity;
+import com.buaa.food.app.AppFragment;
+import com.buaa.food.ui.fragment.StatusFragment;
+import com.hjq.base.FragmentPagerAdapter;
 
 public class HistoryActivity extends AppActivity {
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter<AppFragment<?>> mPagerAdapter;
     protected int getLayoutId() {
         return R.layout.history_record;
     }
 
-    protected void initView() {}
+    protected void initView() {
+        mViewPager = findViewById(R.id.vp_history_pager);
+        mPagerAdapter = new FragmentPagerAdapter<>(this);
+        mPagerAdapter.addFragment(StatusFragment.newInstance());
+        mViewPager.setAdapter(mPagerAdapter);
+    }
 
 
     protected void initData() {}
