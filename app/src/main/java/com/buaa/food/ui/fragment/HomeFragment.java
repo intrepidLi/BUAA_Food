ea3ccab3a4db31768f10974a8587c25e48c5f1c7
Hibiki33 <113701757+Hibiki33@users.noreply.github.com>
2023-12-07 15:19:17 +0800

    add: recommend lines in home fragment

--- app/src/main/java/com/buaa/food/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/buaa/food/ui/fragment/HomeFragment.java
@@ -1,13 +1,27 @@
 package com.buaa.food.ui.fragment;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import com.buaa.food.DataBaseHelper;
+import com.buaa.food.DishPreview;
+import com.buaa.food.http.glide.GlideApp;
+import com.buaa.food.ui.activity.DishDetailActivity;
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.CenterInside;
 import com.gyf.immersionbar.ImmersionBar;
 import com.hjq.base.FragmentPagerAdapter;
 import com.buaa.food.R;
@@ -16,6 +30,11 @@ import com.buaa.food.app.TitleBarFragment;
 import com.buaa.food.ui.activity.HomeActivity;
 import com.buaa.food.ui.adapter.TabAdapter;
 import com.buaa.food.widget.XCollapsingToolbarLayout;
+import com.umeng.commonsdk.debug.D;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 public final class HomeFragment extends TitleBarFragment<HomeActivity>
@@ -32,6 +51,8 @@ public final class HomeFragment extends TitleBarFragment<HomeActivity>
 
     private TabAdapter mTabAdapter;
     private FragmentPagerAdapter<AppFragment<?>> mPagerAdapter;
+
+    private DataBaseHelper dataBaseHelper;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -67,7 +88,8 @@ public final class HomeFragment extends TitleBarFragment<HomeActivity>
         mCollapsingToolbarLayout.setOnScrimsListener(this);
 
         // 初始化三餐推荐
-
+        dataBaseHelper = new DataBaseHelper(this.getContext());
+        initRecommend();
     }
 
     @Override
@@ -136,7 +158,99 @@ public final class HomeFragment extends TitleBarFragment<HomeActivity>
         mTabAdapter.setOnTabListener(null);
     }
 
-    private void initRecommend() {
+    private int breakfastId;
+    private int lunchId;
+    private int dinnerId;
 
+    private void initRecommend() {
+        LinearLayout mBreakfastLayout = findViewById(R.id.ll_breakfast_line);
+        mBreakfastLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(getAttachActivity(), DishDetailActivity.class);
+            intent.putExtra("dishId", breakfastId);
+            startActivity(intent);
+        });
+        AppCompatImageView mBreakfastImage = findViewById(R.id.iv_breakfast_image);
+        AppCompatTextView mBreakfastName = findViewById(R.id.tv_breakfast_name);
+
+        LinearLayout mLunchLayout = findViewById(R.id.ll_lunch_line);
+        mLunchLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(getAttachActivity(), DishDetailActivity.class);
+            intent.putExtra("dishId", lunchId);
+            startActivity(intent);
+        });
+        AppCompatImageView mLunchImage = findViewById(R.id.iv_lunch_image);
+        AppCompatTextView mLunchName = findViewById(R.id.tv_lunch_name);
+
+        LinearLayout mDinnerLayout = findViewById(R.id.ll_dinner_line);
+        mDinnerLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(getAttachActivity(), DishDetailActivity.class);
+            intent.putExtra("dishId", dinnerId);
+            startActivity(intent);
+        });
+        AppCompatImageView mDinnerImage = findViewById(R.id.iv_dinner_image);
+        AppCompatTextView mDinnerName = findViewById(R.id.tv_dinner_name);
+
+        Random random = new Random();
+
+        // randomly select a breakfast from database
+        List<DishPreview> breakfasts = dataBaseHelper.fetchAllBreakfast();
+        DishPreview breakfast = breakfasts.get(random.nextInt(breakfasts.size()));
+        breakfastId = breakfast.getDishId();
+        setRecommendLine(breakfast, mBreakfastImage, mBreakfastName);
+
+        // randomly select a lunch and a dinner from database and collection and history
+        List<DishPreview> allDishes = dataBaseHelper.fetchAllDishes();
+        List<DishPreview> history = dataBaseHelper.fetchHistorys();
+        List<DishPreview> collection = dataBaseHelper.fetchFavorites();
+
+        List<DishPreview> lunches = getRandomListItem(allDishes, 10);
+        lunches.addAll(getRandomListItem(history, 10));
+        lunches.addAll(getRandomListItem(collection, 10));
+        DishPreview lunch = lunches.get(random.nextInt(lunches.size()));
+        lunchId = lunch.getDishId();
+        setRecommendLine(lunch, mLunchImage, mLunchName);
+
+        List<DishPreview> dinners = getRandomListItem(allDishes, 10);
+        dinners.addAll(getRandomListItem(history, 10));
+        dinners.addAll(getRandomListItem(collection, 10));
+        DishPreview dinner = dinners.get(random.nextInt(dinners.size()));
+        dinnerId = dinner.getDishId();
+        setRecommendLine(dinner, mDinnerImage, mDinnerName);
+    }
+
+    private void setRecommendLine(DishPreview dishPreview, AppCompatImageView imageView, AppCompatTextView textView) {
+        byte[] imgByte = dataBaseHelper.getDishImage(dishPreview.getDishId());
+        if (imgByte != null && imgByte.length > 0) {
+            Bitmap imgBitmap = BitmapFactory.decodeByteArray(imgByte, 0, imgByte.length);
+            GlideApp.with(getContext())
+                    .load(imgBitmap)
+                    .placeholder(R.drawable.dish_1)
+                    .error(R.drawable.dish_1)
+                    .transform(new MultiTransformation<>(new CenterInside()))
+                    .into(imageView);
+        } else {
+            GlideApp.with(getContext())
+                    .load(R.drawable.default1)
+                    .placeholder(R.drawable.dish_1)
+                    .error(R.drawable.dish_1)
+                    .transform(new MultiTransformation<>(new CenterInside()))
+                    .into(imageView);
+        }
+        textView.setText(dishPreview.getDishName());
+    }
+
+    public static <T> List<T> getRandomListItem(List<T> list, int count) {
+        List<T> olist = new ArrayList<>();
+        if (list.size() <= count) {
+            return list;
+        } else {
+            Random random = new Random();
+            for (int i = 0; i < count; i++){
+                int intRandom = random.nextInt(list.size() - 1);
+                olist.add(list.get(intRandom));
+                list.remove(list.get(intRandom));
+            }
+            return olist;
+        }
     }
 }
